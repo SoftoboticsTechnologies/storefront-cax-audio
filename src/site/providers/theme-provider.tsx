58b750cca525cbc356d14baa869784cfa221bda8
Kumar Tanay <krtanay97@gmail.com>
2026-09-24 12:12:46 +0530

    initial commit

--- src/site/providers/theme-provider.tsx
+++ src/site/providers/theme-provider.tsx
@@ -13,7 +13,7 @@ export function ThemeProvider({children}: {children: React.ReactNode}) {
     return (
         <NextThemesProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
             scriptProps={scriptProps}
